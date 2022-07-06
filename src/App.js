2b7d3704e05032fca798a4a6ae663eefd4ce3bf8
imcybergeek <imcybergeek@gmail.com>
2022-07-06 16:24:20 +0530

    base functionality added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import wordsList from './WordsList';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const rand = (max) => Math.floor(Math.random() * max);
+  const getNewQuestion = () => {
+    const word = wordsList[rand(wordsList.length)]
+    const substring = rand(word.length - 2)
+    const finalWord = word.substring(substring, substring + 3)
+    return finalWord
+  }
+  const getUserInput = (e) => {
+    if (e.key === 'Enter') {
+      e.target.value.includes(currQ) && wordsList.includes(e.target.value) && setScore(score + 1)
+      e.target.value = null
+      setCurrQ(getNewQuestion)
+    }
+  }
+  const [currQ, setCurrQ] = useState(getNewQuestion)
+  const [score, setScore] = useState(0)
+
+  return <>
+    <p>{currQ}</p>
+    <input type="text" onKeyDown={getUserInput} />
+    <p>{score}</p>
+  </>
 }
 
 export default App;
