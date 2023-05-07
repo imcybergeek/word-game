cd7c0357a154f1afc91a7b829b5743fd0e03e19d
imcybergeek <65075504+imcybergeek@users.noreply.github.com>
2023-05-07 18:29:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -99,7 +99,8 @@ function App() {
           </div>
       </Stats>
     </> : <>
-      <h1>Your Final Score is {score}</h1>
+      <h1>Your Final Score is</h1>
+      <h1>{score}</h1>
       <Button onClick={replay}>Replay</Button>
     </>}
   </div>
